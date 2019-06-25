0dd2295c10b0cbad99b401ac0bf23db209d647ae
Steven <steven.chung96@yahoo.com>
2019-06-25 14:45:22 -0700

    templates and css

--- apps/recipe_app/urls.py
+++ apps/recipe_app/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^$',views.index)
+    url(r'^$',views.index),
+    url(r'^home$', views.home),
+    url(r'^browse$', views.browse),
 ]
--- apps/recipe_app/views.py
+++ apps/recipe_app/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def browse(request):
+    return render(request, 'browse.html')
