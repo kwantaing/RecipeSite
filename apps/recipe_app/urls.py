09ed30045655f7ebe8c34daca6a32502abd6e0c9
Steven <steven.chung96@yahoo.com>
2019-06-26 15:53:37 -0700

    urls.py mods

--- apps/recipe_app/urls.py
+++ apps/recipe_app/urls.py
@@ -2,13 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-<<<<<<< HEAD
     url(r'^login$',views.index),
     url(r'^$',views.test),
     url(r'^home$', views.home),
-=======
-    url(r'^$',views.home),
->>>>>>> ff2b0bab490ca380ca4c4bf0452052926c29001f
     url(r'^browse$', views.browse),
     url(r'^recipe/view/(?P<id>\d+)$',views.showrecipe),
     url(r'^search$',views.search)
