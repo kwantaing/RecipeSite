f01e276708f2a513d2c6b655da15168148bad43d
Steven <steven.chung96@yahoo.com>
2019-06-26 15:23:47 -0700

    login and reg finished

--- apps/recipe_app/urls.py
+++ apps/recipe_app/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    # url(r'^$',views.index),
+    url(r'^login$',views.index),
     url(r'^$',views.test),
     url(r'^home$', views.home),
     url(r'^browse$', views.browse),
