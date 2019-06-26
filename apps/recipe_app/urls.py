6a3cec4dcfdba501b15d1a36a3d2342b1df30fc0
Quentin Ng <qng1997@gmail.com>
2019-06-26 16:08:54 -0700

    merge

--- apps/recipe_app/urls.py
+++ apps/recipe_app/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^$',views.home),
+    url(r'^$',views.index),
+    url(r'^register$',views.register),
+    url(r'^welcome$',views.home),
+    url(r'^login$',views.login),
+    url(r'^logout$',views.logout),
     url(r'^browse$', views.browse),
     url(r'^recipe/view/(?P<id>\d+)$',views.showrecipe),
     url(r'^search$',views.search),
